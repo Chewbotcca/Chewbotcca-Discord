4fc1db6c9a65d6234464b0f4f1e1b0551687a408
Chew <chew@chew.pw>
2020-06-03 20:24:15 -0500

    Add support for command prediction if invalid, and work for aliases

--- src/main/java/pw/chew/Chewbotcca/commands/InfoCommand.java
+++ src/main/java/pw/chew/Chewbotcca/commands/InfoCommand.java
@@ -4,8 +4,11 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import pw.chew.Chewbotcca.util.RestClient;
+
+import java.util.ArrayList;
 
 public class InfoCommand extends Command {
     public InfoCommand() {
@@ -23,19 +26,28 @@ public class InfoCommand extends Command {
         }
         JSONObject data = new JSONObject(RestClient.get("https://chew.pw/chewbotcca/discord/api/command/" + command));
         if(data.has("error")) {
-            event.reply("Invalid command! See <https://chew.pw/chewbotcca/discord/commands> for a list of commands.");
+            JSONArray didYouMean = data.getJSONArray("didYouMean");
+            ArrayList<String> predictions = new ArrayList<>();
+            for(int i = 0; i < didYouMean.length(); i++) {
+                predictions.add(didYouMean.getString(i));
+            }
+            if(predictions.size() > 0) {
+                event.reply("Invalid command! See <https://chew.pw/chewbotcca/discord/commands> for a list of commands. Did you mean? " + String.join(", ", predictions));
+            } else {
+                event.reply("Invalid command! See <https://chew.pw/chewbotcca/discord/commands> for a list of commands.");
+            }
             return;
         }
 
         EmbedBuilder e = new EmbedBuilder()
-                .setTitle("**Info For**: `%^" + command + "`")
+                .setTitle("**Info For**: `%^" + data.getString("command") + "`")
                 .setDescription(data.getString("description"));
         if(!data.isNull("args")) {
             e.addField("Arguments", data.getString("args"), true);
         } else {
             e.addField("Arguments", "No arguments", true);
         }
-        if(!data.has("aliases")) {
+        if(!data.isNull("aliases")) {
             e.addField("Aliases", data.getString("aliases"), true);
         } else {
             e.addField("Aliases", "No aliases", true);
