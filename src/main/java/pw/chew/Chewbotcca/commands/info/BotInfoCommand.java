9242e65fa281f3f870f9212dc923f6c05ca6dab4
Chew <chew@chew.pw>
2020-06-25 13:48:39 -0500

    Use raw json restclient calls for Top.gg Bot Info Sorry nik I needed more info

--- src/main/java/pw/chew/Chewbotcca/commands/info/BotInfoCommand.java
+++ src/main/java/pw/chew/Chewbotcca/commands/info/BotInfoCommand.java
@@ -4,17 +4,16 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
-import org.awaitility.core.ConditionTimeoutException;
-import org.discordbots.api.client.entity.Bot;
+import net.dv8tion.jda.api.entities.User;
+import org.json.JSONObject;
 import pw.chew.Chewbotcca.Main;
+import pw.chew.Chewbotcca.util.RestClient;
 
 import java.awt.*;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static org.awaitility.Awaitility.await;
 
 public class BotInfoCommand extends Command {
     public BotInfoCommand() {
@@ -43,48 +42,48 @@ public class BotInfoCommand extends Command {
             list = args[1].toLowerCase();
         }
         switch (list) {
-            case "dbl", "top.gg", "topgg" -> commandEvent.reply(gatherTopggInfo(botId).build());
+            case "dbl", "top.gg", "topgg" -> commandEvent.reply(gatherTopggInfo(botId, commandEvent).build());
             default -> commandEvent.reply("Invalid Bot List! Possible: ```dbl, topgg```");
         }
     }
 
-    private EmbedBuilder gatherTopggInfo(String id) {
-        AtomicReference<Bot> bot = new AtomicReference<>();
-        Main.topgg.getBot(id).whenComplete(((bot1, throwable) -> bot.set(bot1)));
-        try {
-            await().atMost(5, TimeUnit.SECONDS).until(() -> bot.get() != null);
-        } catch(ConditionTimeoutException e) {
-            return new EmbedBuilder().setTitle("Error!").setDescription("Timed out finding the bot, does it exist?");
-        }
+    private EmbedBuilder gatherTopggInfo(String id, CommandEvent event) {
+        JSONObject bot = new JSONObject(RestClient.get("https://top.gg/api/bots/" + id, Main.getProp().getProperty("dbl")));
 
         EmbedBuilder e = new EmbedBuilder();
         e.setTitle("Bot Information");
 
         String certified;
-        if(bot.get().isCertified()) {
+        if(bot.getBoolean("certifiedBot")) {
             certified = "https://cdn.discordapp.com/emojis/392249976639455232.png";
         } else {
-            certified = String.format("https://cdn.discordapp.com/avatars/%s/%s.%s", id, bot.get().getAvatar(), "png");
+            certified = String.format("https://cdn.discordapp.com/avatars/%s/%s.%s", id, bot.getString("avatar"), "png");
         }
 
-        e.setAuthor(bot.get().getUsername() + "#" + bot.get().getDiscriminator(), "https://top.gg/bot/" + id, certified);
+        e.setAuthor(bot.getString("username") + "#" + bot.getString("discriminator"), "https://top.gg/bot/" + id, certified);
 
-        e.setThumbnail(String.format("https://cdn.discordapp.com/avatars/%s/%s.%s", id, bot.get().getAvatar(), "png"));
+        e.setThumbnail(String.format("https://cdn.discordapp.com/avatars/%s/%s.%s", id, bot.getString("avatar"), "png"));
 
-        e.setDescription(bot.get().getShortDescription());
+        e.setDescription(bot.getString("shortdesc"));
 
-        List<String> owners = new ArrayList<>();
+        e.addField("Bot ID", bot.getString("id"), true);
 
-        e.addField("Bot ID", bot.get().getId(), true);
+        if(bot.has("server_count"))
+            e.addField("Server Count", String.valueOf(bot.getInt("server_count")), true);
+        else
+            e.addField("Server Count", "Unknown", true);
 
-        e.addField("Server Count", String.valueOf(bot.get().getServerCount()), true);
+        e.addField("Prefix", "`" + bot.getString("prefix") + "`", true);
 
-        e.addField("Prefix", "`" + bot.get().getPrefix() + "`", true);
+        e.addField("Library", bot.getString("lib"), true);
 
-        e.addField("Points", "This Month: " + bot.get().getMonthlyPoints() + "\n" +
-                "All Time: " + bot.get().getPoints(), true);
+        e.addField("Points", "This Month: " + bot.getInt("monthlyPoints") + "\n" +
+                "All Time: " + bot.getInt("points"), true);
 
-        List<String> tags = bot.get().getTags();
+        List<String> tags = new ArrayList<>();
+        for(Object tag : bot.getJSONArray("tags")) {
+            tags.add((String) tag);
+        }
 
         if(tags.isEmpty()) {
             e.addField("Tags", "None", true);
@@ -92,24 +91,46 @@ public class BotInfoCommand extends Command {
             e.addField("Tags", String.join(", ", tags), true);
         }
 
+        List<String> owners = new ArrayList<>();
+        for(Object owner : bot.getJSONArray("owners")) {
+            User user = event.getJDA().getUserById((String) owner);
+            if(user == null)
+                owners.add((String) owner);
+            else
+                owners.add(user.getAsTag());
+        }
+
+        if(owners.isEmpty()) {
+            e.addField("Owners", "None", true);
+        } else {
+            e.addField("Owners", String.join(", ", owners), true);
+        }
+
         List<CharSequence> links = new ArrayList<>();
         links.add("[Bot Page](https://top.gg/bot/" + id + ")");
         links.add("[Vote](https://top.gg/bot/" + id + "/vote)");
-        if(!bot.get().getInvite().equals(""))
-            links.add("[Invite](" + bot.get().getInvite() + ")");
-        if(!bot.get().getWebsite().equals(""))
-            links.add("[Website](" + bot.get().getWebsite() + ")");
-        if(bot.get().getWebsite() != null)
-            links.add("[Support](" + bot.get().getWebsite() + ")");
+        if(!bot.getString("invite").equals(""))
+            links.add("[Invite](" + bot.getString("invite") + ")");
+        if(!bot.getString("website").equals(""))
+            links.add("[Website](" + bot.getString("website") + ")");
+        if(!bot.getString("support").equals(""))
+            links.add("[Support Server](https://discord.gg/" + bot.getString("support") + ")");
+        if(!bot.getString("github").equals(""))
+            links.add("[GitHub](" + bot.getString("github") + ")");
 
         e.addField("Links", String.join("\n", links), true);
 
         e.setFooter("Bot added");
-        e.setTimestamp(bot.get().getApprovalTime());
+        e.setTimestamp(dateParser(bot.getString("date")));
 
         e.setColor(Color.decode("#43B581"));
 
         return e;
+    }
+
+    public OffsetDateTime dateParser(String date) {
+        DateTimeFormatter inputFormat = DateTimeFormatter.ofPattern("uuuu-MM-dd'T'HH:mm:ss.SSSX");
+        return OffsetDateTime.parse(date, inputFormat);
     }
 }
 
