761c603f37dabc9ae0c56335060a847bba276882
Chew <chew@chew.pw>
2020-09-20 13:43:50 -0500

    Add notice if user attempts to %^help command

--- src/main/java/pw/chew/chewbotcca/commands/about/HelpCommand.java
+++ src/main/java/pw/chew/chewbotcca/commands/about/HelpCommand.java
@@ -20,6 +20,7 @@ import com.jagrosh.jdautilities.command.Command;
 import com.jagrosh.jdautilities.command.CommandEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 
 // %^help command
 public class HelpCommand extends Command {
@@ -33,17 +34,25 @@ public class HelpCommand extends Command {
     @Override
     protected void execute(CommandEvent commandEvent) {
         // Reply with embed
-        commandEvent.reply(new EmbedBuilder()
-                .setTitle("Welcome to the Chewbotcca Discord Bot")
-                .setColor(0xd084)
-                .setDescription("""
+        String notice = null;
+        if (!commandEvent.getArgs().isEmpty()) {
+            notice = "Psst, to view more info about a command, use `%^info command`!";
+        }
+        MessageEmbed embed = new EmbedBuilder()
+            .setTitle("Welcome to the Chewbotcca Discord Bot")
+            .setColor(0xd084)
+            .setDescription("""
                     Chewbotcca is a multi-purpose, semi-functional, almost always online, Discord bot!
 
                     This bot is powered by [SkySilk Cloud Services](https://www.skysilk.com/ref/4PRQpuQraD)!""")
-                .addField("Commands", "You can find all my commands [here](https://chew.pw/chewbotcca/discord/commands)", true)
-                .addField("Invite me!", "You can invite me to your server with [this link](https://discord.com/oauth2/authorize?client_id=604362556668248095&scope=bot&permissions=0).", true)
-                .addField("Help Server", "Click [me](https://discord.gg/UjxQ3Bh) to join the help server.", true)
-                .addField("More Bot Stats", "Run `%^stats` to see more stats!", true)
-        .build());
+            .addField("Commands", "You can find all my commands [here](https://chew.pw/chewbotcca/discord/commands)", true)
+            .addField("Invite me!", "You can invite me to your server with [this link](https://discord.com/oauth2/authorize?client_id=604362556668248095&scope=bot&permissions=0).", true)
+            .addField("Help Server", "Click [me](https://discord.gg/UjxQ3Bh) to join the help server.", true)
+            .addField("More Bot Stats", "Run `%^stats` to see more stats!", true)
+            .build();
+        if (notice == null)
+            commandEvent.getChannel().sendMessage(embed).queue();
+        else
+            commandEvent.getChannel().sendMessage(notice).embed(embed).queue();
     }
 }
