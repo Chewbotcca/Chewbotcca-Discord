79332560c6032e18525d4d2ea1d6a0f48a79e2bb
Chew <chew@chew.pw>
2020-11-24 22:51:49 -0600

    Add gateway ping to PingCommand response

--- src/main/java/pw/chew/chewbotcca/commands/about/PingCommand.java
+++ src/main/java/pw/chew/chewbotcca/commands/about/PingCommand.java
@@ -37,7 +37,10 @@ public class PingCommand extends Command {
         long time = commandEvent.getMessage().getTimeCreated().toInstant().toEpochMilli();
         // Send a "Checking ping" message and calculate the difference between this message and the %^ping message
         commandEvent.getChannel().sendMessage(new EmbedBuilder().setDescription("Checking ping..").build()).queue((msg) -> {
-            EmbedBuilder eb = new EmbedBuilder().setDescription("Ping is " + (msg.getTimeCreated().toInstant().toEpochMilli() - time) + "ms");
+            EmbedBuilder eb = new EmbedBuilder().setDescription(
+                "Ping is " + (msg.getTimeCreated().toInstant().toEpochMilli() - time) + "ms\n" +
+                "Gateway Ping is " + commandEvent.getJDA().getGatewayPing() + "ms\n"
+            );
             msg.editMessage(eb.build()).queue();
         });
     }
