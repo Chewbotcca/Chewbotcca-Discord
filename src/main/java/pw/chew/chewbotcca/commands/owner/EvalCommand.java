098ca842c5cf27168dd44ca163d9f64aec4d6330
Chew <chew@chew.pw>
2023-01-15 10:42:34 -0600

    Make owner commands work in DM

--- src/main/java/pw/chew/chewbotcca/commands/owner/EvalCommand.java
+++ src/main/java/pw/chew/chewbotcca/commands/owner/EvalCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2022 Chewbotcca
+ * Copyright (C) 2023 Chewbotcca
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -42,6 +42,7 @@ public class EvalCommand extends Command {
     public EvalCommand() {
         this.name = "eval";
         this.ownerCommand = true;
+        this.guildOnly = false;
     }
 
     @Override
