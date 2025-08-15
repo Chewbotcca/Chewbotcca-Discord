4ef0d2a6d84a4a1f32c3aa134e5cf57be565e45f
Chew <chew@chew.pw>
2025-08-15 17:40:01 -0500

    feat: use components v2 in mc user response
    Also fix bug with labymod lol

--- src/main/java/pw/chew/chewbotcca/commands/minecraft/MCUserSubCommand.java
+++ src/main/java/pw/chew/chewbotcca/commands/minecraft/MCUserSubCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Chewbotcca
+ * Copyright (C) 2025 Chewbotcca
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,9 +18,14 @@ package pw.chew.chewbotcca.commands.minecraft;
 
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.components.actionrow.ActionRow;
+import net.dv8tion.jda.api.components.buttons.Button;
+import net.dv8tion.jda.api.components.container.Container;
+import net.dv8tion.jda.api.components.section.Section;
+import net.dv8tion.jda.api.components.separator.Separator;
+import net.dv8tion.jda.api.components.textdisplay.TextDisplay;
+import net.dv8tion.jda.api.components.thumbnail.Thumbnail;
+import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.utils.TimeFormat;
@@ -34,15 +39,17 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 
-// %^mcuser command
+/**
+ * <h2><code>/minecraft user</code> Command</h2>
+ *
+ * <a href="https://help.chew.pro/bots/discord/chewbotcca/commands/minecraft#user-subcommand">Docs</a>
+ */
 public class MCUserSubCommand extends SlashCommand {
 
     public MCUserSubCommand() {
         this.name = "user";
         this.help = "Looks up a Minecraft user and returns their profile";
-        this.aliases = new String[]{"namemc"};
-        this.botPermissions = new Permission[]{Permission.MESSAGE_EMBED_LINKS};
-        this.guildOnly = false;
+        this.contexts = new InteractionContextType[]{InteractionContextType.GUILD, InteractionContextType.BOT_DM, InteractionContextType.PRIVATE_CHANNEL};
         this.options = Collections.singletonList(
             new OptionData(OptionType.STRING, "user", "The user name or UUID to lookup").setRequired(true)
         );
@@ -52,13 +59,14 @@ public class MCUserSubCommand extends SlashCommand {
     protected void execute(SlashCommandEvent event) {
         String name = event.optString("user", "");
         try {
-            event.deferReply().queue(interactionHook -> interactionHook.editOriginalEmbeds(gatherData(name)).queue());
+            event.deferReply()
+                .queue(hook -> hook.editOriginalComponents(gatherData(name)).useComponentsV2().queue());
         } catch (IllegalArgumentException e) {
             event.reply(e.getMessage()).setEphemeral(true).queue();
         }
     }
 
-    private MessageEmbed gatherData(String name) {
+    private Container gatherData(String name) {
         String uuid;
         // Get profile info
         if (name.length() == 32 || name.length() == 36) {
@@ -81,26 +89,45 @@ public class MCUserSubCommand extends SlashCommand {
         }
         JSONObject profile = RestClient.get("https://laby.net/api/v2/user/" + uuid + "/get-profile").asJSONObject();
         JSONArray history = profile.getJSONArray("username_history");
+
         // Find recent names and when they were changed
         StringBuilder names = new StringBuilder();
         for(int i = history.length() - 1; i >= 0; i--) {
             JSONObject data = history.getJSONObject(i);
             String time;
-            if(!data.isNull("changed_at")) {
+            if (!data.isNull("changed_at") && data.getString("changed_at").length() > 4) {
                 OffsetDateTime at = MiscUtil.dateParser(data.getString("changed_at"), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
                 time = TimeFormat.DATE_TIME_SHORT.format(at);
+            } else if (!data.isNull("changed_at")) {
+                time = data.getString("changed_at");
             } else {
                 time = "Original";
             }
+
             String username = data.getString("name");
             names.append("`").append(username).append("` - ").append(time).append("\n");
         }
-        // Return info
-        return (new EmbedBuilder()
-            .setTitle("Minecraft Profile Information for " + history.getJSONObject(history.length() - 1).getString("name"))
-            .setDescription("[NameMC Profile](https://namemc.com/profile/" + uuid + ")")
-            .setThumbnail("https://minotar.net/helm/" + uuid)
-            .addField("Name History", names.toString(), true)
-            .build());
+
+        String currentName = profile.getString("username");
+
+        return Container.of(
+            Section.of(
+                // avatar
+                Thumbnail.fromUrl("https://minotar.net/helm/" + uuid),
+                TextDisplay.of("# Minecraft Profile Information\n ## User: " + currentName)
+            ),
+
+            Separator.createDivider(Separator.Spacing.SMALL),
+
+            TextDisplay.of("Name History\n" + names),
+
+            Separator.createDivider(Separator.Spacing.SMALL),
+
+            // buttons to view online
+            ActionRow.of(
+                Button.link("https://namemc.com/profile/" + uuid, "View on NameMC"),
+                Button.link("https://laby.net/@" + uuid, "View on LabyMod")
+            )
+        );
     }
 }
