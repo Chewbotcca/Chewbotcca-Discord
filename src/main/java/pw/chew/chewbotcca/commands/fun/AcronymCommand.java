20d03b0c81207d076ff559242aa26ce64cc4128b
Chew <chew@chew.pw>
2025-12-04 23:17:34 -0600

    feat: clean up acronym command, allow non-letters and use gson

--- src/main/java/pw/chew/chewbotcca/commands/fun/AcronymCommand.java
+++ src/main/java/pw/chew/chewbotcca/commands/fun/AcronymCommand.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Chewbotcca
+ * Copyright (C) 2025 Chewbotcca
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,48 +16,46 @@
  */
 package pw.chew.chewbotcca.commands.fun;
 
-import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.command.SlashCommand;
 import com.jagrosh.jdautilities.command.SlashCommandEvent;
-import net.dv8tion.jda.api.interactions.InteractionContextType;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
-import pw.chew.chewbotcca.objects.Memory;
+import pw.chew.chewbotcca.util.CommandContext;
+import pw.chew.chewbotcca.util.RestClient;
 
 import java.util.Collections;
 
-// %^acronym command
+/**
+ * <h2><code>/acronym</code> Command</h2>
+ *
+ * <a href="https://help.chew.pro/bots/discord/chewbotcca/commands/acronym">Docs</a>
+ */
 public class AcronymCommand extends SlashCommand {
-
     public AcronymCommand() {
         this.name = "acronym";
         this.help = "Jokingly tries to fill in an acronym.";
-        this.contexts = new InteractionContextType[]{InteractionContextType.GUILD, InteractionContextType.BOT_DM, InteractionContextType.PRIVATE_CHANNEL};
+        this.contexts = CommandContext.GLOBAL;
         this.options = Collections.singletonList(
-            new OptionData(OptionType.STRING, "acronym", "The acronym to fill").setRequired(true)
+            new OptionData(OptionType.STRING, "acronym", "The acronym to fill", true)
         );
     }
 
     @Override
     protected void execute(SlashCommandEvent event) {
-        // Get acronym and send if the acronym is valid
-        try {
-            String acronym = event.optString("acronym", "");
-            String phrase = Memory.getChewAPI().generateAcronym(acronym);
-            event.reply("Acronym for " + acronym + " is " + phrase).queue();
-        } catch (IllegalArgumentException e) {
-            event.reply("Args must only contain letters!").setEphemeral(true).queue();
+        // Get acronym strip out non letters
+        String acronym = event.optString("acronym", "");
+        acronym = acronym.replaceAll("[^A-Za-z]", "");
+
+        // Check for blank acronyms
+        if (acronym.isBlank()) {
+            event.reply("You must provide some letters!").setEphemeral(true).queue();
+            return;
         }
+
+        // Get phrase
+        String phrase = RestClient.get("https://api.chew.pro/acronym/" + acronym).asGsonObject(Acronym.class).phrase();
+        event.reply("Acronym for %s is %s".formatted(acronym, phrase)).queue();
     }
 
-    @Override
-    protected void execute(CommandEvent event) {
-        // Get acronym and send if the acronym is valid
-        try {
-            String phrase = Memory.getChewAPI().generateAcronym(event.getArgs());
-            event.reply("Acronym for " + event.getArgs() + " is " + phrase);
-        } catch (IllegalArgumentException e) {
-            event.reply("Args must only contain letters!");
-        }
-    }
+    private record Acronym(String phrase) { }
 }
